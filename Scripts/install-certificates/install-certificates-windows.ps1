008aa8091e28378ac2cffa72223599522b8a5809
Demian Marty <demian.marty@4tecture.ch>
2023-03-27 15:33:32 +0200

    enable check for cert type

--- Scripts/install-certificates/install-certificates-windows.ps1
+++ Scripts/install-certificates/install-certificates-windows.ps1
@@ -55,20 +55,20 @@ try {
 
     [System.IO.File]::WriteAllBytes($tempFilePath, [System.Convert]::FromBase64String($base64cert))
     Write-Host "Certificate saved"
-        
-    # if ($certificatePassword) {
-    Write-Host "Use Certificate Password: $($certificatePassword)"
 
-    #     $securePassword = ConvertTo-SecureString -String $certificatePassword -AsPlainText -Force
-    #     $certificatePassword = "deleted"
+    if ($certificatePassword) {
+        Write-Host "Certificate Password is set, import certificate as PFX (X.509)"
 
-    #     Get-ChildItem -Path "$($tempFilePath)" | Import-PfxCertificate -CertStoreLocation "Cert:\$($certStoreLocation)" -Exportable -Password "$($securePassword)"
-    #     Write-Host "Certificate $($certificateName) added to the $($certStoreLocation) store succesfully."
-    # }
-    #else {
-    Get-ChildItem -Path "$($tempFilePath)" | Import-Certificate -CertStoreLocation "Cert:\$($certStoreLocation)"
-    Write-Host "Certificate $($certificateName) added to the $($certStoreLocation) store succesfully."
-    # }
+        $securePassword = ConvertTo-SecureString -String $certificatePassword -AsPlainText -Force
+        $certificatePassword = "deleted"
+
+        Get-ChildItem -Path "$($tempFilePath)" | Import-PfxCertificate -CertStoreLocation "Cert:\$($certStoreLocation)" -Exportable -Password "$($securePassword)"
+        Write-Host "PFX Certificate $($certificateName) added to the $($certStoreLocation) store succesfully."
+    }
+    else {
+        Get-ChildItem -Path "$($tempFilePath)" | Import-Certificate -CertStoreLocation "Cert:\$($certStoreLocation)"
+        Write-Host "Certificate $($certificateName) added to the $($certStoreLocation) store succesfully."
+    }
 
     Remove-Item -Path "$($tempFilePath)" -Force
     Write-Host "Deleted the temp file $($tempFilePath)"
